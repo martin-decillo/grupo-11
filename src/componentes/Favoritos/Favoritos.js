e3657207a2a8bf12a5af8e6df3fb639d543dba86
Carvi Apps <carvi.apps@gmail.com>
2026-04-18 16:57:24 -0300

    favoritas

--- src/componentes/Favoritos/Favoritos.js
+++ src/componentes/Favoritos/Favoritos.js
@@ -12,12 +12,37 @@ class Favoritas extends Component(){
         enviarFormulario(event){
         event.preventDefault();
          
-        let usuario = {
-            nombre: this.state.nombre,
-            gmail: this.state.gmail,
-            contraseña: this.state.contraseña
+        let favorita = {
+            favorita: this.state.favorita,
         };
         
         localStorage.setItem("favorita", JSON.stringify());
     }
+      componentDidMount(){
+    const id = this.props.match.params.id;
+
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=830571fa1c832cffccac2021413e6933&language=es-ES`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ pelicula: data });
+        console.log(data)
+      })
+      .catch(error => console.log(error));
+  }
+  render(){
+    return(
+    <section>
+        <Header/>
+        {this.state.favorita === null ? (
+          <h2>Cargando...</h2>
+        ) : (
+          <div>
+
+          </div>
+        )}
+        <Footer/>
+      </section>
+    );
+  }
+
 }
