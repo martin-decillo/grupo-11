5b3860e250a6e87943a8e0dfa1aaf15b9ecff965
martin-decillo <decillomartin@gmail.com>
2026-04-19 13:16:13 -0300

    acomodo favortitas

--- src/screens/Favotitas/Favoritas.js
+++ src/screens/Favotitas/Favoritas.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../../componentes/Header/Header";
 import Footer from "../../componentes/Footer/Footer";
+import Peliculapopular from "../../componentes/Peliculapopular/Peliculapopular";
 
 class Favoritas extends Component {
   constructor(props) {
@@ -21,31 +22,36 @@ class Favoritas extends Component {
     });
   }
 
-  render() {
-    return (
-      <section>
-        <Header />
-        <h1>Mis favoritas</h1>
+render() {
+  return (
+    <div className="Peliculaspopulares">
+      <Header />
 
-        {this.state.favoritas.length === 0 ? (
-          <h2>No tenés películas favoritas guardadas</h2>
+      <h3>Mis favoritas</h3>
+
+      {
+        this.state.favoritas.length === 0 ? (
+          <h1>No tenés películas favoritas guardadas</h1>
         ) : (
-          this.state.favoritas.map((pelicula, idx) => (
-            <article key={pelicula.id || idx}>
-              <img
-                src={`https://image.tmdb.org/t/p/w342/${pelicula.poster_path}`}
-                alt={pelicula.title}
-              />
-              <h2>{pelicula.title}</h2>
-              <p>{pelicula.overview}</p>
-            </article>
+          this.state.favoritas.map((elemento, idx) => (
+            <Peliculapopular
+              key={elemento.id + idx}
+              title={elemento.title}
+              language={elemento.original_language}
+              adult={elemento.adult}
+              img={`https://image.tmdb.org/t/p/w500${elemento.poster_path}`}
+              poster_path={elemento.poster_path}
+              id={elemento.id}
+              descripcion={elemento.overview}
+            />
           ))
-        )}
+        )
+      }
 
-        <Footer />
-      </section>
-    );
-  }
+      <Footer />
+    </div>
+  );
+}
 }
 
 export default Favoritas;
